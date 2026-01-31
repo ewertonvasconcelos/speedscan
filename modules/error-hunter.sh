082d99f468067acc44f5e4bbb698b5e82e0d481d
Ewerton Vasconcelos <ewerton.vasconcelos@exemplo.com>
2026-01-31 17:20:47 -0300

    FAXINA GERAL: Mantendo apenas SpeedScan.sh e README.md

--- modules/error-hunter.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "--- Erros Críticos (Últimas 2h) ---"
-journalctl --since "2 hours ago" -p 3..0 --no-pager
--- modules/fazer_touchpad.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "--- Resolvendo problema do Mouse Fantasma ---"
-sudo modprobe -r i2c_hid_acpi
-sudo modprobe i2c_hid_acpi
-echo "Touchpad reiniciado com sucesso!"
