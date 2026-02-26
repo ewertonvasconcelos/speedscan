a53bf6034be85bef3da00d4987350c413342ea12
Ewerton Vasconcelos <ewertonvasconcelosdossantos@gmail.com>
2026-02-26 08:06:59 -0300

    Atualização automática do SpeedScan

---
+++ run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+distrobox enter speedscan-dev -- python3 /home/ewerton/speedscan/core/speedscan_app.py
---
+++ test.py
@@ -0,0 +1,5 @@
+import customtkinter as ctk
+root = ctk.CTk()
+root.title("Teste")
+root.geometry("300x200")
+root.mainloop()
